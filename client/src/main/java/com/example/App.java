c3f09d3ca352314b6f8cbb1f8d1bd275a06e3549
lai2-24 <lai2-24@itismeucci.com>
2023-10-17 13:53:19 +0200

    creato il client

--- client/src/main/java/com/example/App.java
+++ client/src/main/java/com/example/App.java
@@ -1,13 +1,38 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            //CREO UN SOCKET
+            Socket socket = new Socket("localhost", 3000);
+            //CREO IL TUBO DA DOVE MI VIENE LA RISPOSTA DAL SERVER
+            BufferedReader in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+            //CREO IL TUBO DA DOVE MANDARE AL SERVER
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            //CREO IL PRENDI DA TASTIERA
+            BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
+
+            do{
+                //CHIEDO IL NUMERO ALL'UTENTE
+                System.out.println("Indovina il numero generato:");
+                String tentativo = tastiera.readLine();
+                //INVIO IL TENTATIVO AL SERVER
+                out.writeBytes(tentativo + "\n");
+                //PRENDO RISPOSTA DAL SERVER E STAMPO
+                String str = in.readLine();
+                System.out.println("Il numero [" + tentativo + "]" + str);
+            }while(true);
+        }
+        catch (Exception e){
+            System.out.println("Errore");
+        }
     }
 }
